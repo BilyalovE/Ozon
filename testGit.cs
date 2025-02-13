549a63434b9e33e7fb32285c709e8fdb14545c08
Bilyalov <eldar2117@gmail.com>
2025-02-13 19:18:33 +0300

    new changes

--- testGit.cs
+++ testGit.cs
@@ -1,7 +1,9 @@
+// тестовый модуль
 void Print(string name){
 	Console.WriteLine("Hello, " + name);
 };
 
+
 void Print(string firstName, string lastName){
-	Console.WriteLine($"Hello, {lastName} {firstName}");
+	
 };
